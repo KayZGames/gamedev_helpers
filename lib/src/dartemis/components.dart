361e2b8b5e76d0151cd07fb5e02c3e134b577e89
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2013-11-30 12:30:21 +0100

    added SoundSystem

---
+++ lib/src/dartemis/components.dart
@@ -0,0 +1,6 @@
+part of gamedev_helpers;
+
+class Sound extends Component {
+  final String clipName;
+  Sound(this.clipName);
+}
--- lib/src/dartemis/systems.dart
+++ lib/src/dartemis/systems.dart
@@ -31,3 +31,18 @@ class FpsRenderingSystem extends VoidEntitySystem {
     ctx.fillText('FPS: ${fps.toStringAsFixed(2)}', 5, 5);
   }
 }
+
+class SoundSystem extends EntityProcessingSystem {
+  ComponentMapper<Sound> sm;
+  AudioHelper helper;
+  SoundSystem(this.helper) : super(Aspect.getAspectForAllOf([Sound]));
+
+  initialize() {
+    sm = new ComponentMapper<Sound>(Sound, world);
+  }
+
+  processEntity(Entity e) {
+    helper.playClip(sm.get(e).clipName);
+    e.deleteFromWorld();
+  }
+}
