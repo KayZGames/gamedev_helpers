d72b7c961f50a3d2a48a844971e69249530a145f
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2015-05-03 21:37:47 +0200

    default handling for left/right/up/down

--- lib/src/client/dartemis/systems/input.dart
+++ lib/src/client/dartemis/systems/input.dart
@@ -4,6 +4,7 @@ abstract class GenericInputHandlingSystem extends EntityProcessingSystem {
   /// to prevent scrolling
   var preventDefaultKeys = new Set.from([KeyCode.UP, KeyCode.DOWN, KeyCode.LEFT, KeyCode.RIGHT, KeyCode.SPACE]);
   var keyState = <int, bool>{};
+  var unpress = <int, bool>{};
   GenericInputHandlingSystem(Aspect aspect) : super(aspect);
 
   @override
@@ -14,8 +15,18 @@ abstract class GenericInputHandlingSystem extends EntityProcessingSystem {
 
   void handleInput(KeyboardEvent event, bool pressed) {
     keyState[event.keyCode] = pressed;
+    if (!pressed && unpress[event.keyCode] == true) {
+      unpress[event.keyCode] = false;
+    }
     if (preventDefaultKeys.contains(event.keyCode)) {
       event.preventDefault();
     }
   }
+
+  bool get left => isPressed(KeyCode.A) || isPressed(KeyCode.LEFT);
+  bool get right => isPressed(KeyCode.D) || isPressed(KeyCode.RIGHT);
+  bool get up => isPressed(KeyCode.W) || isPressed(KeyCode.UP);
+  bool get down => isPressed(KeyCode.S) || isPressed(KeyCode.DOWN);
+
+  bool isPressed(int keyCode) => keyState[keyCode] == true && unpress[keyCode] != true;
 }
