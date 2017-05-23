5b79c0bfdcff380633ec11e89272433e66b79604
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2017-05-23 14:37:06 +0200

    removed simple_audio dependency

--- lib/gamedev_helpers.dart
+++ lib/gamedev_helpers.dart
@@ -11,10 +11,8 @@ import 'dart:js' as js;
 
 import 'package:gamedev_helpers/gamedev_helpers_shared.dart';
 export 'package:gamedev_helpers/gamedev_helpers_shared.dart';
-import 'package:simple_audio/simple_audio.dart';
 
 part 'src/client/assets.dart';
-part 'src/client/audio.dart';
 part 'src/client/game_base.dart';
 part 'src/client/rendering.dart';
 
@@ -23,20 +21,11 @@ part 'src/client/dartemis/systems/debugging.dart';
 part 'src/client/dartemis/systems/input.dart';
 part 'src/client/dartemis/systems/rendering_context2d.dart';
 part 'src/client/dartemis/systems/rendering_webgl.dart';
-part 'src/client/dartemis/systems/sound.dart';
 
 class GameHelper {
   final String _libName;
-  AudioHelper _audioHelper;
   AudioContext _audioContext;
   GameHelper(this._libName, this._audioContext);
-
-  AudioHelper get audioHelper {
-    if (null == _audioHelper) {
-      _audioHelper = new AudioHelper(_libName);
-    }
-    return _audioHelper;
-  }
 
   /// Loads achievements.json.
   /// Format {name: desciption, otherName: otherDescription}
@@ -56,21 +45,4 @@ class GameHelper {
   /// Loads shader/[vShaderFile].vert and shader/[fShaderFile].frag.
   Future<ShaderSource> loadShader(String vShaderFile, String fShaderFile) =>
       _loadShader(_libName, vShaderFile, fShaderFile);
-}
-
-class AudioHelper {
-  final String _libName;
-  final AudioManager _audioManager;
-  AudioHelper(String libName)
-      : _libName = libName,
-        _audioManager = _createAudioManager(libName);
-
-  /// Loads .ogg and .mp3 files with [names] from sfx-directory.
-  Future<List<AudioClip>> loadAudioClips(List<String> names) =>
-      _loadAudioClips(_audioManager, names);
-
-  /// Plays clip on default source
-  void playClip(String clipName) {
-    _audioManager.playClipFromSource('default', clipName);
-  }
 }
